2523f11ccbef5cafba8337f7155a94c9a6f081ab
Vasu Gupta <androfly.official@gmail.com>
2017-10-03 16:59:04 +0530

    NodeJs Work

--- Express/routes/index.js
+++ Express/routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var passData = "Passing Data Through router" ;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Express AndroFly' ,
-    name : "Vasu Gupta"
+    name : "Vasu Gupta" ,
+    passData : passData
   }); // It says that we want to use index as view and passing title as Express
 });
 
