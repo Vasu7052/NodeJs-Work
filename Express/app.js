73c9046c1399b801175f30cfbc0ef129a0efbdf9
Vasu Gupta <androfly.official@gmail.com>
2017-10-03 16:04:39 +0530

    NodeJs Work

--- Express/app.js
+++ Express/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var logger = require('morgan'); // For showing logs
+var cookieParser = require('cookie-parser'); // For Handling Cookies
+var bodyParser = require('body-parser'); // For parsing a text/json body
 
-var index = require('./routes/index');
+var index = require('./routes/index'); // routes are brains behind everything
 
-var app = express();
+var app = express(); // Connect framework
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,16 +20,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', index); // Looking up for paths
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler          // This block of error handler is used at the time of development
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Not Found');  // Error Handler
   err.status = 404;
-  next(err);
+  next(err); // Passes to next error handler
 });
 
-// error handler
+// error handler                      // This block of error handler is used when the project is live
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
