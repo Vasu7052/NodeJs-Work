30bfe1b5003176b3e7c45a068aa769e3eca10b51
Vasu Gupta <androfly.official@gmail.com>
2017-10-03 21:16:16 +0530

    NodeJs Work

--- BuildingFirstApi/app.js
+++ BuildingFirstApi/app.js
@@ -33,6 +33,7 @@ app.get("/" , function (req , res) {
     res.send("Hello World") ;
 });
 
+// <================================= GENRES =========================
 app.get('/api/genres/', function(req, res){
     Genre.getGenres(function(err, genres){
     if(err){
@@ -42,6 +43,41 @@ app.get('/api/genres/', function(req, res){
 });
 });
 
+app.post('/api/genres', function(req, res){
+    var genre = req.body;
+    Genre.addGenre(genre, function(err, genre){
+    if(err){
+        throw err;
+    }
+    res.json(genre);
+});
+});
+
+app.put('/api/genres/:_id', function(req, res){
+    var id = req.params._id;
+    var genre = req.body;
+    Genre.updateGenre(id, genre, {}, function(err, genre){
+    if(err){
+        throw err;
+    }
+    res.json(genre);
+});
+});
+
+app.delete('/api/genres/:_id', function(req, res){
+    var id = req.params._id;
+    Genre.removeGenre(id, function(err, genre){
+    if(err){
+        throw err;
+    }
+    res.json(genre);
+});
+});
+
+// <=======================================================================
+
+
+// <================================= BOOKS ===============================
 app.get('/api/books/', function(req, res){
     Book.getBooks(function(err, books){
         if(err){
@@ -59,6 +95,39 @@ app.get('/api/books/:_id', function(req, res){
     res.json(book);
 });
 });
+
+app.post('/api/books', function(req, res){
+    var book = req.body;
+    Book.addBook(book, function(err, book){
+    if(err){
+        throw err;
+    }
+    res.json(book);
+});
+});
+
+app.put('/api/books/:_id', function(req, res){
+    var id = req.params._id;
+    var book = req.body;
+    Book.updateBook(id, book, {}, function(err, book){
+    if(err){
+        throw err;
+    }
+    res.json(book);
+});
+});
+
+app.delete('/api/books/:_id', function(req, res){
+    var id = req.params._id;
+    Book.removeBook(id, function(err, book){
+    if(err){
+        throw err;
+    }
+    res.json(book);
+});
+});
+
+// <=======================================================================
 
 
 // catch 404 and forward to error handler
--- BuildingFirstApi/routes/genre.js
+++ BuildingFirstApi/routes/genre.js
@@ -6,23 +6,6 @@ const genreSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    genre:{
-        type: String,
-        required: true
-    },
-    description:{
-        type: String
-    },
-    author:{
-        type: String,
-        required: true
-    },
-    publisher:{
-        type: String
-    },
-    pages:{
-        type: String
-    },
     create_date:{
         type: Date,
         default: Date.now
@@ -34,4 +17,25 @@ const Genre = module.exports = mongoose.model('Genre', genreSchema , "genre");
 // Get Genres
 module.exports.getGenres = function(callback, limit){
     Genre.find(callback).limit(limit);
+}
+
+// Add Genre
+module.exports.addGenre = function(genre, callback){
+    Genre.create(genre, callback);
+}
+
+// Update Genre
+module.exports.updateGenre = function(id, genre, options, callback){
+    var query = {_id: id};
+    var update = {
+        name: genre.name
+    }
+    Genre.findOneAndUpdate(query, update, options, callback);
+}
+
+
+// Delete Genre
+module.exports.removeGenre = function(id, callback){
+    var query = {_id: id};
+    Genre.remove(query, callback);
 }
