efbe3b7a3375e2bac28ef58d75b97b2f99b4301d
Vasu Gupta <androfly.official@gmail.com>
2017-10-03 13:53:31 +0530

    NodeJs Work

--- CoreModules/public/app.js
+++ CoreModules/public/app.js
@@ -2,3 +2,10 @@ var fs = require("fs") ; // File System Module
 
 fs.writeFileSync("corn.txt" , "Corn is Good and sweet") ;
 console.log(fs.readFileSync("corn.txt").toString());
+
+var path = require("path") ;
+var websiteHome = "Desktop/Vasu//index.html" ;
+var websiteAbout = "Desktop/Vasu/about.html" ;
+
+console.log(path.normalize(websiteHome));
+console.log(path.normalize(websiteAbout));
