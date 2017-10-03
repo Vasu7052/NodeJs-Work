c3b201d228d143e3ba9d7b6ae727c4b3285f2b65
Vasu Gupta <androfly.official@gmail.com>
2017-10-03 15:08:13 +0530

    NodeJs Work

---
+++ ServerConnectFramework/public/server.js
@@ -0,0 +1,8 @@
+var connect = require("connect");
+var http = require("http") ;
+
+var app = connect();
+
+http.createServer(app).listen(8888) ;
+
+console.log("Server is running...");
--- ServerFrameworkConnect/public/server.js
+++
@@ -1,3 +0,0 @@
-var connect = require("connect");
-var http = require("http") ;
-
