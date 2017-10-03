ab513b0f2ce147df1fa8f298cca99c56b52d6e6a
Vasu Gupta <androfly.official@gmail.com>
2017-10-03 15:18:38 +0530

    NodeJs Work

--- ServerConnectFramework/public/server.js
+++ ServerConnectFramework/public/server.js
@@ -3,6 +3,30 @@ var http = require("http") ;
 
 var app = connect();
 
+/* function doFirst(request , response , next) {
+    console.log("Candy1") ;
+    next() ;
+}
+
+function doSecond(request , response , next) {
+    console.log("Candy2") ;
+    next() ;
+}
+
+app.use(doFirst) ;
+app.use(doSecond) ;    */
+
+function profile(request , response) {
+    console.log("User Requested profile page")
+}
+
+function forum(request , response) {
+    console.log("User Requested forum page")
+}
+
+app.use("/profile" , profile) ;
+app.use("/forum" , forum) ;
+
 http.createServer(app).listen(8888) ;
 
 console.log("Server is running...");
