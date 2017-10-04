0b192df33dafe43813e164ae43f4dd6fcce5ece5
Vasu Gupta <androfly.official@gmail.com>
2017-10-04 22:52:30 +0530

    NodeJs Work

--- FirstApiProject-Android/app/src/main/java/com/example/vasu/firstapiproject_android/MainActivity.java
+++ FirstApiProject-Android/app/src/main/java/com/example/vasu/firstapiproject_android/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.vasu.firstapiproject_android;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.example.vasu.firstapiproject_android.Model.Genre;
 import com.example.vasu.firstapiproject_android.Model.GenreResponse;
+import com.example.vasu.firstapiproject_android.SlideTabView.SlidingTabLayout;
+import com.example.vasu.firstapiproject_android.SlideTabView.ViewPagerAdapter;
 import com.example.vasu.firstapiproject_android.helper.ApiClient;
 import com.example.vasu.firstapiproject_android.helper.ApiInterface;
 import com.google.gson.JsonElement;
@@ -18,13 +22,43 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*List<Genre> genre = response.body().getResults();
-                Toast.makeText(MainActivity.this, "Number of movies received: " + genre.size() , Toast.LENGTH_SHORT).show();*/
+    Toolbar toolbar;
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[]={"Add Books","Add Genre"};
+    int Numboftabs =2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.colorPrimary);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
 
