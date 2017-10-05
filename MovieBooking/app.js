b4b79f360810cbc68a4284ff01ca62902de338f4
Vasu Gupta <androfly.official@gmail.com>
2017-10-05 13:10:03 +0530

    NodeJs Work

--- MovieBooking/app.js
+++ MovieBooking/app.js
@@ -5,8 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose  = require("mongoose") ;
+mongoose.connect('mongodb://localhost/movieBooking', { useMongoClient: true }) ;
+
 var index = require('./routes/index');
 var users = require('./routes/users');
+var Movies = require('./routes/movies');
 
 var app = express();
 
@@ -24,6 +28,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+// =============================================== Movies =============================================
+
+app.get("/api/movies/all/" , function (req,res) {
+    Movies.getAllMovies(function (err,movies) {
+        if(err){
+            throw err;
+        }
+        var temp = {"allMovies" : movies};
+        res.json(temp);
+    })
+});
+
+
+// ====================================================================================================
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- MovieBooking/routes/movies.js
+++ MovieBooking/routes/movies.js
@@ -2,6 +2,36 @@ const mongoose = require("mongoose") ;
 
 const moviesSchema = mongoose.Schema({
 
-
+    title : {
+        type : String ,
+        required : true
+    },year : {
+        type : Number ,
+        required : true
+    },genre : {
+        type : String ,
+        required : true
+    },description : {
+        type : String ,
+        required : true
+    },director : {
+        type : String ,
+        required : true
+    },stars : {
+        type : String ,
+        required : true
+    },imdbRating : {
+        type : Number ,
+        required : true
+    },create_date:{
+        type: Date,
+        default: Date.now
+    }
 
 });
+
+const Movie = module.exports = mongoose.model("Movie" , moviesSchema , "movieList") ;
+
+module.exports.getAllMovies = function (callback,limit) {
+    Movie.find(callback).limit(limit) ;
+};
